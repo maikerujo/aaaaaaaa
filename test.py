35bc1aa1756db43562222d50343020b388e4de4e
aaawwwws <adeqa1@icloud.com>
2023-08-06 14:01:37 +0900

    aaa

---
+++ test.py
@@ -0,0 +1,8 @@
+A = [
+    {"名前": "じょaり", "メールアドレス": "jorijori@unko.com"},
+    {"名前": "あで", "メールアドレス": "ade@unko.com"},
+    {"名前": "アイリス", "メールアドレス": "iris@tinpo.com"},
+]
+
+for List in A:
+    print(List)
